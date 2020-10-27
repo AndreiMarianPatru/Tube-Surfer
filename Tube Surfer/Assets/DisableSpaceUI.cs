dcdae8f8949922e676a5e8658d1b6c348ebd8d55
Andrei Marian Patru <andreiuttzzumarian00@gmail.com>
2020-10-27 09:30:34 +0000

    tutorial UI

---
+++ Tube Surfer/Assets/DisableSpaceUI.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class DisableSpaceUI : MonoBehaviour
+{
+    [SerializeField]
+    private TextMeshProUGUI SpaceTXT;
+    [SerializeField]
+    private TextMeshProUGUI qeTXT;
+    // Start is called before the first frame update
+    void Start()
+    {
+        qeTXT.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+  
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            SpaceTXT.enabled = false;
+            qeTXT.enabled = true;
+        }
+    }
+
+    //void OnTriggerExit(Collider other)
+    //{
+    //    if (other.CompareTag("Player"))
+    //    {
+    //        customimage.enabled = false;
+    //    }
+    //}
+
+    //void OnTriggerEnter(Collider other)
+    //{
+    //    Debug.Log("deletetxt");
+    //    if (other.gameObject.tag == "Player")
+    //    {
+    //        Debug.Log("deletetxt");
+    //        Destroy(GameObject.Find("SpaceTXT"));
+    //    }
+    //}
+}
