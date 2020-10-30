6a1ae5a96029b81c5aca91888653c6ca464b702f
Andrei Marian Patru <andreiuttzzumarian00@gmail.com>
2020-10-30 17:41:10 +0000

    framerate

--- Tube Surfer/Assets/scripts/StartScenes.cs
+++ Tube Surfer/Assets/scripts/StartScenes.cs
@@ -5,10 +5,11 @@ using UnityEngine.SceneManagement;
 
 public class StartScenes : MonoBehaviour
 {
+   
     // Start is called before the first frame update
     void Start()
     {
-        
+         Application.targetFrameRate = -1;
     }
 
     // Update is called once per frame
