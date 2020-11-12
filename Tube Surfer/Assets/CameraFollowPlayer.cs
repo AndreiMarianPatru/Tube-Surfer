8a755a224728fe0223ec665dbf896f95c69da0ef
Andrei Marian Patru <andreiuttzzumarian00@gmail.com>
2020-11-12 20:54:24 +0000

    small update

---
+++ Tube Surfer/Assets/CameraFollowPlayer.cs
@@ -0,0 +1,14 @@
+﻿ using UnityEngine;
+ using System.Collections;
+ 
+ public class CameraFollowPlayer : MonoBehaviour {
+  
+  public GameObject player;
+  
+  // Update is called once per frame
+  void Update () {
+  
+  this.gameObject.transform.position = new Vector3(player.gameObject.transform.position.x,player.gameObject.transform.position.y,player.gameObject.transform.position.z);
+  
+  }
+}
