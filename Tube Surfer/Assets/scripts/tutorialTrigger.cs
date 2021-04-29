99def76e80a07e7542ff5b5a7ec892ed2204196b
Andrei Marian Patru <andreiuttzzumarian00@gmail.com>
2021-04-29 15:38:51 +0100

    tutorial end screen

--- Tube Surfer/Assets/scripts/tutorialTrigger.cs
+++ Tube Surfer/Assets/scripts/tutorialTrigger.cs
@@ -5,24 +5,44 @@ using UnityEngine.SceneManagement;
 
 public class tutorialTrigger : MonoBehaviour
 {
+    
+    public Canvas endscreen;
     // Start is called before the first frame update
-    GameObject button;
     void Start()
     {
-        
+        endscreen.enabled = false;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    public void EndGame()
+    {
+        Time.timeScale = 0;
+      
+        endscreen.enabled = true;
+    }
+    public void Mainmenu()
+    {
+        SceneManager.LoadScene("Main Menu");
+
+    }
+    public void playagain()
+    {
+        SceneManager.UnloadSceneAsync(SceneManager.GetActiveScene().buildIndex);
+        Resources.UnloadUnusedAssets();
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex, LoadSceneMode.Single);
     }
     private void OnTriggerEnter(Collider other)
     {
         
         if (other.gameObject.tag=="Player")
         {
-            SceneManager.LoadScene("Main Menu");
+            EndGame();
 
         }
     }
